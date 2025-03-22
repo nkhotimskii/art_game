6d88f1239c45e89bc82c6bf744e095a4f7cc0a24
nkhotimskii <nkhotimskii@gmail.com>
2025-03-23 01:08:35 +0300

    API endpoints added

--- art_game/urls.py
+++ art_game/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+from art_game import views
+
+router = routers.DefaultRouter()
+router.register(r'questions', views.QuestionViewSet)
+router.register(r'answers', views.AnswerViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
 ]
