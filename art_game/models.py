808ac0f9e7c5d0ba868bdcef9d62a4fb4bb59863
nkhotimskii <nkhotimskii@gmail.com>
2025-03-23 00:51:29 +0300

    Models added

---
+++ art_game/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Question(models.Model):
+
+    image = models.CharField(
+        max_length=50
+    )
+
+    def __str__(self):
+        return self.image
+
+
+class Answer(models.Model):
+
+    answer = models.CharField(
+        max_length=500
+    )
+    is_correct = models.BooleanField()
+    question_id = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.answer
