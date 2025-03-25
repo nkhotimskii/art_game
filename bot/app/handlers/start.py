7264aeb2d090ceb61c4e69173002e840ada07ecc
nkhotimskii <nkhotimskii@gmail.com>
2025-03-25 16:09:53 +0300

    Image sending on start added

--- bot/app/handlers/start.py
+++ bot/app/handlers/start.py
@@ -1,9 +1,11 @@
+import os
+
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 import aiohttp
 
-from constants import API_URL
+from constants import API_URL, IMAGES_FOLDER
 from markups.answers import get_answers_markup
 from router import router
 from states import Questions
@@ -18,8 +20,14 @@ async def start_handler(msg: Message, state: FSMContext) -> None:
             questions = await response.json()
     await state.update_data(questions=questions)
     question = questions[0]
+    image_name = question["image"]
+    image_path = os.path.join(IMAGES_FOLDER, image_name)
+    image = FSInputFile(image_path)
     answers = question["answers"]
-    answers_markup = get_answers_markup(answers)
+    answers_markup = get_answers_markup(answers)    
+    await msg.answer_photo(
+        image
+    )
     await msg.answer(
         text="Что это за картина?",
         reply_markup=answers_markup
